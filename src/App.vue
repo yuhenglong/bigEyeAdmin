ba9226fd9ed224650dc006a885a82dd7efe17793
henglong <907161303@qq.com>
2020-11-26 17:48:09 +0800

    更新说明

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <login/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import login from './components/login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    login
+  }
+}
+</script>
+
+<style>
+#app,body,html {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="login">
+    <el-form
+      :model="ruleForm"
+      status-icon
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="用户名" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass">
+        <el-input
+          type="password"
+          v-model="ruleForm.pass"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >登录</el-button
+        >
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  components: {},
+  data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    return {
+      ruleForm: {
+        pass: "",
+        name: ""
+      },
+      rules: {
+        pass: [{ required: true,validator: validatePass, trigger: "blur" }],
+        name: [{ required: true, message: '请输入正确用户名', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.login {
+  width: 100%;
+  height: 100%;
+  background: url(../assets/bgc.jpg) no-repeat center center;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-wrap: wrap;
+}
+.demo-ruleForm {
+  width: 400px;
+  height: auto;
+}
+
+</style>
